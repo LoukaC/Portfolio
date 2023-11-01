c7417ee02f9fc6078de93083ad65c72f67e99ca3
Louka <louka.cipolat@yahoo.fr>
2023-11-01 16:06:27 +0000

    section project contact et organisation

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ window.addEventListener('scroll', function() {
     var moonWidth = moon.offsetWidth;
     var endPosition = windowWidth - moonWidth;
 
-    moon.style.right = -(scrollPosition * 0.3) + 'px';
+    moon.style.right = -(scrollPosition * 0.05) + 'px';
 
     if (scrollPosition > endPosition) {
         moon.style.opacity = 0;
@@ -30,3 +30,33 @@ var typed = new Typed("#multiple-text", {
     smartBackspace: true,
     loop: true
 });
+
+
+// scroll section active link
+
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll =() =>{
+    sections.forEach(sec =>{
+        let top= window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
+
+
+// header fixe
+
+let header = document.querySelector('header');
+
+header.classList.toggle('fixed', window.scrollY > 100)
+
